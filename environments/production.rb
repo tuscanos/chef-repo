d883418e03c13a518363b03b9a25a4c161e0769f
tuscanos <shonal.machado@gmail.com>
2015-11-12 17:10:16 -0800

    Added environments

---
+++ environments/production.rb
@@ -0,0 +1,3 @@
+name "production"
+description "Production Environment"
+override_attributes :webserver => { :version => "1.0.0" }
---
+++ environments/staging.rb
@@ -0,0 +1,3 @@
+name "staging"
+description "Staging Environment"
+override_attributes :webserver => { :version => "0.2.0" }
